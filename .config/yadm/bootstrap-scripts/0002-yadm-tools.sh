c36ba462177fdf9ff18880d6f73bbb6f376948b2
TJ Hoplock <t.hoplock@gmail.com>
2026-02-06 17:28:32 -0500

    ref(yadm): switch dotfile bootstrap to devbox, remove asdf
    Signed-off-by: TJ Hoplock <t.hoplock@gmail.com>

--- .config/yadm/bootstrap-scripts/0002-yadm-tools.sh
+++ .config/yadm/bootstrap-scripts/0002-yadm-tools.sh
@@ -1,28 +1,48 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-echo "[$(basename $0)]: initializing tools"
+script_name="$(basename "$0")"
 
-echo "[$(basename $0)]: initializing z script"
+echo "[$script_name]: initializing tools"
+
+# z script - directory jumping utility
+echo "[$script_name]: initializing z script"
 zscript_dir="$HOME/github/z"
-mkdir -p $zscript_dir
-git clone https://github.com/rupa/z.git $zscript_dir
+if [[ -d "$zscript_dir/.git" ]]; then
+    echo "[$script_name]: z script already cloned, updating"
+    git -C "$zscript_dir" pull
+else
+    echo "[$script_name]: cloning z script"
+    mkdir -p "$zscript_dir"
+    git clone https://github.com/rupa/z.git "$zscript_dir"
+fi
 
-echo "[$(basename $0)]: initializing asdf"
-asdf_dir="$HOME/github/asdf"
-mkdir -p $asdf_dir
-git clone https://github.com/asdf-vm/asdf.git $asdf_dir
-source $asdf_dir/asdf.sh
+# devbox
+echo "[$script_name]: ensuring devbox is installed"
+command -v devbox &> /dev/null || curl -fsSL https://get.jetify.com/devbox | bash
 
-asdf_tools=(
-    "golang"
-    "golangci-lint"
+# devbox global packages
+echo "[$script_name]: initializing devbox global packages"
+devbox_packages=(
+    "llama-cpp"
+    "awscli"
+    "doctl"
+    "helm"
+    "kind"
     "kubectl"
+    "minikube"
+    "nodejs"
     "python"
+    "terraform"
+    "yarn"
+    "go"
+    "fzf"
+    "go-task"
 )
 
-for tool in "${asdf_tools[@]}"; do
-    echo "[$(basename $0)]: initializing ${tool} with latest version"
-    asdf plugin-add "${tool}"
-    asdf install "${tool}" latest
-    asdf global "${tool}" latest
+for package in "${devbox_packages[@]}"; do
+    echo "[$script_name]: adding devbox global package: $package"
+    devbox global add "${package}@latest"
 done
+
+echo "[$script_name]: tools initialization complete"
